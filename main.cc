4531307c1a90b2d8a06a9afc46af5f61c5dc58cc
handsome boy <100855117+5p6@users.noreply.github.com>
2023-02-11 22:14:24 +0800

    Update main.cc

--- main.cc
+++ main.cc
@@ -4,46 +4,7 @@
 #include<Eigen/Dense>
 #include<opencv2/opencv.hpp>
 #include"optim.hpp"
-
-void test2(){
-    Mine::Vec3 xpoints[100];
-    Mine::Vec3 ypoints[100];
-
-    Eigen::Matrix3d K;
-    K<<330,0,150,0,330,200,0,0,1;
-    Eigen::Matrix<double,2,2,Eigen::RowMajor> m;
-    cv::RNG rng;
-    m<<100,100,100,100;
-    for(int i = 0 ;i<100;i++){
-        double x[3] ={
-            rng.uniform(0.1,12.5),
-            rng.uniform(0.1,12.5),
-            rng.uniform(0.1,12.5)
-        };
-        double zaos = rng.gaussian(1.0);
-        xpoints[i] = Mine::Vec3(x[0] + zaos,x[1]+ 2*zaos,x[2]- zaos);
-        Mine::Vec3 temp = K*xpoints[i];
-        ypoints[i] = temp;
-    }
-    ceres::Problem problem;
-    for(int i = 0;i<100;i++){
-        problem.AddResidualBlock(
-            Mine::MatOptim::create(xpoints[i],ypoints[i]),
-            nullptr,
-            m.data()
-        );
-    }
-    ceres::Solver::Options options;
-    options.linear_solver_type = ceres::DENSE_QR;
-    options.max_num_iterations = 200;
-    options.minimizer_progress_to_stdout = true;
-    options.gradient_tolerance = 1e-20;
-    ceres::Solver::Summary summary;
-    ceres::Solve(options,&problem,&summary);
-    std::cout<<summary.BriefReport()<<std::endl;
-    std::cout<< m <<std::endl;
-}
-void test10(){
+void test(){
     Mine::Vec3 xpoints[100];
     Mine::Vec3 ypoints[100];
 
@@ -85,6 +46,6 @@ void test10(){
 int main(int argc,char* argv[])
 {
     google::InitGoogleLogging(argv[0]);
-    test10();
+    test();
     return 1;
 }
